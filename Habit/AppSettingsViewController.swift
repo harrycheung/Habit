82fe10f5736099c35e06a30bc7ecb6ca232f98ac
Harry Cheung <harry.cheung@gmail.com>
2015-08-31 14:29:36 -0700

    Fixed bug related to huge negative velocities

--- Habit/AppSettingsViewController.swift
+++ Habit/AppSettingsViewController.swift
@@ -69,30 +69,28 @@ class AppSettingsViewController: UIViewController, ColorPickerDataSource, ColorP
   }
   
   @IBAction func panning(recognizer: UIPanGestureRecognizer) {
-    let translation = recognizer.translationInView(view)
-    if translation.y > 0 {
-      view.frame = CGRectMake(0, translation.y, view.frame.width, view.frame.height)
-      darkenView!.frame = CGRectMake(0, 0, view.frame.width, paddingView.bounds.height + translation.y)
-      let panPercentage = 1 - translation.y / (view.frame.height - paddingView.bounds.height)
-      darkenView!.alpha = panPercentage * AppSettingsTransition.DarkenAlpha
-      close.alpha = max(1 - translation.y / AppSettingsViewController.PanMinimum, 0)
-      
-      if recognizer.state == .Ended {
-        if translation.y < AppSettingsViewController.PanMinimum {
-          UIView.animateWithDuration(AppSettingsTransition.TransitionDuration,
-            delay: 0,
-            usingSpringWithDamping: AppSettingsTransition.SpringDamping,
-            initialSpringVelocity: AppSettingsTransition.SpringVelocity,
-            options: AppSettingsTransition.AnimationOptions,
-            animations: {
-              self.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
-              self.darkenView!.frame = CGRectMake(0, 0, self.view.frame.width, self.paddingView.bounds.height)
-              self.darkenView!.alpha = AppSettingsTransition.DarkenAlpha
-              self.close.alpha = 1
-            }, completion: nil)
-        } else {
-          performSegueWithIdentifier("SettingsUnwind", sender: self)
-        }
+    let movement = max(recognizer.translationInView(view).y, 0)
+    view.frame = CGRectMake(0, movement, view.frame.width, view.frame.height)
+    darkenView!.frame = CGRectMake(0, 0, view.frame.width, paddingView.bounds.height + movement)
+    let panPercentage = 1 - movement / (view.frame.height - paddingView.bounds.height)
+    darkenView!.alpha = panPercentage * AppSettingsTransition.DarkenAlpha
+    close.alpha = max(1 - movement / AppSettingsViewController.PanMinimum, 0)
+    
+    if recognizer.state == .Ended || recognizer.state == .Cancelled {
+      if movement < AppSettingsViewController.PanMinimum {
+        UIView.animateWithDuration(AppSettingsTransition.TransitionDuration,
+          delay: 0,
+          usingSpringWithDamping: AppSettingsTransition.SpringDamping,
+          initialSpringVelocity: AppSettingsTransition.SpringVelocity,
+          options: AppSettingsTransition.AnimationOptions,
+          animations: {
+            self.view.frame = CGRectMake(0, 0, self.view.frame.width, self.view.frame.height)
+            self.darkenView!.frame = CGRectMake(0, 0, self.view.frame.width, self.paddingView.bounds.height)
+            self.darkenView!.alpha = AppSettingsTransition.DarkenAlpha
+            self.close.alpha = 1
+          }, completion: nil)
+      } else {
+        performSegueWithIdentifier("SettingsUnwind", sender: self)
       }
     }
   }
@@ -122,23 +120,26 @@ class AppSettingsViewController: UIViewController, ColorPickerDataSource, ColorP
   }
   
   @IBAction func autoSkipChanged(sender: AnyObject) {
-    HabitApp.autoSkip = autoSkip.on
     if autoSkip.on {
+      let darkenHeight = self.paddingView.bounds.height - self.autoSkipHeight.constant
       autoSkipHeight.priority = HabitApp.LayoutPriorityHigh
       UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut,
         animations: {
-          self.darkenView!.frame = CGRectMake(
-            0, 0, self.view.frame.width, self.paddingView.bounds.height - self.autoSkipHeight.constant)
+          self.darkenView!.frame = CGRectMake(0, 0, self.view.frame.width, darkenHeight)
           self.view.layoutIfNeeded()
-        }, completion: nil)
+        }, completion: { finished in
+          HabitApp.autoSkip = true
+      })
     } else {
+      let darkenHeight = self.paddingView.bounds.height + self.autoSkipHeight.constant
       autoSkipHeight.priority = HabitApp.LayoutPriorityLow
       UIView.animateWithDuration(0.3, delay: 0, options: .CurveEaseOut,
         animations: {
-          self.darkenView!.frame = CGRectMake(
-            0, 0, self.view.frame.width, self.paddingView.bounds.height + self.autoSkipHeight.constant)
+          self.darkenView!.frame = CGRectMake(0, 0, self.view.frame.width, darkenHeight)
           self.view.layoutIfNeeded()
-        }, completion: nil)
+        }, completion: { finished in
+          HabitApp.autoSkip = false
+      })
     }
   }
   
@@ -163,8 +164,8 @@ class AppSettingsViewController: UIViewController, ColorPickerDataSource, ColorP
   }
   
   @IBAction func autoSkipDelayChanged(sender: AnyObject) {
-    HabitApp.autoSkipDelay = Int(autoSkipStepper.value)
     autoSkipDelay.text = autoSkipDelayString(Int(autoSkipStepper.value))
+    HabitApp.autoSkipDelay = Int(autoSkipStepper.value)
   }
   
 }
