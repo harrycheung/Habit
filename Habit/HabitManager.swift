c8f068ad2d77a83d070ef154f22d8f467f59f1d2
Harry Cheung <harry.cheung@gmail.com>
2015-09-28 13:42:10 -0700

    Update habits after becoming active

---
+++ Habit/HabitManager.swift
@@ -0,0 +1,31 @@
+//
+//  HabitManager.swift
+//  Habit
+//
+//  Created by harry on 9/28/15.
+//  Copyright © 2015 Harry Cheung. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+class HabitManager {
+  
+  static func updateHabits() {
+    do {
+      let now = NSDate()
+      let request = NSFetchRequest(entityName: "Habit")
+      let habits = try HabitApp.moContext.executeFetchRequest(request) as! [Habit]
+      for habit in habits {
+        habit.update(now)
+        if HabitApp.autoSkip && !habit.neverAutoSkipBool {
+          habit.skip(before: NSDate(timeInterval: HabitApp.autoSkipDelayTimeInterval, sinceDate: now))
+        }
+      }
+      try HabitApp.moContext.save()
+    } catch let error as NSError {
+      NSLog("updateHabits failed: \(error.localizedDescription)")
+    }
+  }
+  
+}
